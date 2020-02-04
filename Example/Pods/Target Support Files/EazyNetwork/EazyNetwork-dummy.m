e2d5bb3fcbd8f57445469eb47303684d5ffd0e37
Чингиз Куандык <k.chyngiz@gmail.com>
2020-02-04 18:33:51 +0600

    Create bare project via pod create lib.

---
+++ Example/Pods/Target Support Files/EazyNetwork/EazyNetwork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EazyNetwork : NSObject
+@end
+@implementation PodsDummy_EazyNetwork
+@end
---
+++ Example/Pods/Target Support Files/Pods-EazyNetwork_Example/Pods-EazyNetwork_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EazyNetwork_Example : NSObject
+@end
+@implementation PodsDummy_Pods_EazyNetwork_Example
+@end
